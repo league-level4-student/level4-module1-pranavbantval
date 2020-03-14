19163c2918a0d9686b701f1cc0fcae8999aa62cf
League Student <league@iMac-22.attlocal.net>
2020-03-13 18:29:40 -0700

    snake

--- src/_06_Snake/Snake.java
+++ src/_06_Snake/Snake.java
@@ -25,7 +25,8 @@ public class Snake {
 
 	public void feed() {
 		// 1. add a new SnakeSegment object to the snake
-		snake.add(new SnakeSegment(snake.get(snake.size()-1).getLocation(), BODY_SIZE));
+		snake.add(new SnakeSegment(head.getLocation(), BODY_SIZE));
+		System.out.println("feed");
 	}
 
 	public Location getHeadLocation() {
@@ -60,32 +61,15 @@ public class Snake {
 		// 2a. Update each snake segment to the location of the segment
 		// in front of it.
 		for (int i = snake.size() - 1; i>0; i--) {
-			Location l = snake.get(i-1).getLocation();
-			if(currentDirection.equals(Direction.RIGHT)) {
-				l.x=l.x-1;
-				System.out.println("R");
-			}
-			else if(currentDirection.equals(Direction.LEFT)) {
-				l.x=l.x+1;
-				System.out.println("L");
-			}
-			else if(currentDirection.equals(Direction.UP)) {
-				l.y=l.y-1;
-				System.out.println("U");
-			}
-			else if(currentDirection.equals(Direction.DOWN)) {
-				l.y=l.y+1;
-				System.out.println("D");
-			}
-				snake.get(i).setLocation(l);
-				
-
+		SnakeSegment s = snake.get(i);
+		SnakeSegment t = snake.get(i-1);
+		s.setLocation(t.getLocation());	
 		}
 
 		// 3.set the location of the head to the new location calculated in step 1
 
 		head.setLocation(Newloc);
-
+System.out.println("head change");
 		// 4. set canMove to true
 		canMove = true;
 	}
@@ -151,9 +135,10 @@ else {
 		// 1. complete the method so it returns true if the head is located
 		// in the same location as any other body segment
 for (int i = 1; i < snake.size(); i++) {
-	if(getHeadLocation().equals(snake.get(i).getLocation())) {
-		System.out.println(":(");
-		return true;
+	if(head.getLocation().equals(snake.get(i).getLocation())) {
+		System.out.println(getHeadLocation().x + ", " + getHeadLocation().y);
+		System.out.println(snake.get(i).getLocation().x + ", " + snake.get(i).getLocation().y);
+		//return true;
 	}
 }
 		return false;
